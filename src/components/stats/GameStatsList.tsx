710baf890646f32f665eabf4d5effb6f8ced96fa
Antti <hellsten.antti@gmail.com>
2026-07-22 23:23:57 +0300

    visualized stats with gauge

--- src/components/stats/GameStatsList.tsx
+++ src/components/stats/GameStatsList.tsx
@@ -2,8 +2,10 @@ import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import GamesIcon from "@mui/icons-material/Games";
+import { Gauge } from "@mui/x-charts/Gauge";
 
 import { GameStatsListProps } from "../../types/types.js";
+import Box from "@mui/material/Box";
 
 const GameStatsList = ({ statType }: GameStatsListProps) => {
 
@@ -16,9 +18,31 @@ const GameStatsList = ({ statType }: GameStatsListProps) => {
               <GamesIcon color="primary" fontSize="small" aria-hidden="true" />
             </ListItemIcon>
             {stat.name}:&nbsp;
-            <strong>{stat.value}</strong>.
+            <strong>{stat.value}</strong>
             {stat.percentage !== undefined &&
-              <>&nbsp;({stat.percentage}%)</>
+            <>
+              <Box
+                sx={{
+                  display: "flex",
+                  alignItems: "flex-end",
+                  height: "1.5rem",
+                }}
+              >
+                <Gauge
+                  width={60}
+                  height={40}
+                  startAngle={-90}
+                  endAngle={90}
+                  value={stat.percentage}
+                  sx={{
+                    "& .MuiGauge-valueText": {
+                      transform: "translateY(-0.3rem)",
+                      fontSize: "0.75rem",
+                    },
+                  }}
+                />
+              </ Box>%
+            </>
             }
           </ListItem>
         );
